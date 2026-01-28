2e16570f32270f070be71acc9351a99b66396101
coastalpr <78867068+coastalpr@users.noreply.github.com>
2026-01-28 10:09:57 -0400

    Update weather_dashboard_public_shared.py

--- weather_dashboard_public_shared.py
+++ weather_dashboard_public_shared.py
@@ -147,8 +147,8 @@ c4.metric("🧭 Dirección del Viento (º)", f"{wind_direction_cardinal(latest.w
 # In the last column (c5), display UV index and description on the same line with matching font size
 with c5:
     # Use st.markdown to display both UV index and description on the same line
-    st.markdown(f"<p  style='color:{"black"}; font-size: 1rem; display:inline;'>☀️ Índice UV</p>", unsafe_allow_html=True)
-    st.markdown(f"<p style='color:{"black"}; font-size: 2.2rem; display:inline;'>{latest.uv:.1f}</p>", unsafe_allow_html=True)
+    st.markdown(f"<p  style='color:{"black"}; font-size: 0.7rem; display:inline;'>☀️ Índice UV</p>", unsafe_allow_html=True)
+    st.markdown(f"<p style='color:{color}; font-size: 2.2rem; display:inline;'>{latest.uv:.1f}</p>", unsafe_allow_html=True)
     st.markdown(f"<p style='color:{color}; font-size: 1.0rem; display:inline;'> {description}</p>", unsafe_allow_html=True)
 
 
@@ -451,6 +451,7 @@ st.plotly_chart(fig, use_container_width=True)
 # -----------------------------
 st.markdown("---")
 st.caption("Powered by Streamlit • Plotly • NetCDF • Python")
+
 
 
 
