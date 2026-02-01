6545089e054511abe5617dd4e9a29ac996859848
coastalpr <78867068+coastalpr@users.noreply.github.com>
2026-02-01 15:29:15 -0400

    Update weather_dashboard_public_shared.py

--- weather_dashboard_public_shared.py
+++ weather_dashboard_public_shared.py
@@ -397,92 +397,6 @@ fig.update_layout(
 st.plotly_chart(fig, use_container_width=True)
 
 
-#
-
-# Plot each arrow as a short line starting at (time, speed)
-#for t, y, deltax, deltay, c, spd, wd in zip(time, speed, dx, dy, colors, speed, direction):
-#    fig.add_trace(go.Scatter(
-#        x=[t, t + np.timedelta64(int(deltax*3600*24), 's')],
-#        y=[y, y + deltay],
-#        mode="lines",
-#        line=dict(color=c, width=3),
-#        hovertemplate=f"Velocidad: {spd:.1f} kts<br>Dirección: {wd:.0f}°<extra></extra>",
-#        showlegend=False
-#    ))
-
-# Layout
-#fig.update_layout(
-#    title="Viento: Velocidad y Dirección",
-#    xaxis=dict(
-#        title="Hora",
-#        tickvals=ticks,
-#        ticktext=tick_labels,
-#        tickangle=0,
-#        showspikes=True,
-#        spikecolor="gray",
-#    ),
-#    yaxis=dict(title="Velocidad del viento (kts)"),
-#    hovermode="x unified",
-#    height=450,
-#    width=1100
-#)
-
-#st.plotly_chart(fig, width="stretch")
-
-#fig = px.line(df, x="Hora", y="wind_avg", title="Velocidad del Viento",labels={"wind_avg": "Velocidad del Viento (kts)"})
-
-# Hover: only y-value, no colored box
-#fig.update_traces(
-    #hovertemplate='%{y:.1f} °F<extra></extra>',
-#)
-
-# Layout
-#fig.update_layout(
-    #hovermode="x unified",
-    #xaxis=dict(
-        #tickvals=ticks,
-        #ticktext=tick_labels,   # date + hour for all ticks
-        #tickangle=90,
-        #showline=False,         # no black line
-        #showspikes=True,       # no vertical blue line
-        #spikecolor='rgb(128,128,128)',
-        #range=[start_date, end_date],
-        #side='bottom'
-    #),
-    #yaxis_title="Velocidad del Viento (kts)",
-    #showlegend=False
-#)
-
-#st.plotly_chart(fig, use_container_width=True)
-
-## ----------------------------------------
-# Wind Direction
-## ----------------------------------------
-fig = px.line(df, x="Hora", y="wind_direction", title="Dirección del Viento",labels={"wind_direction": "Dirección del Vienton(º)"})
-
-fig.update_traces(
-    hovertemplate='%{y:.1f} °F<extra></extra>',
-)
-
-# Layout
-fig.update_layout(
-    hovermode="x unified",
-    xaxis=dict(
-        tickvals=ticks,
-        ticktext=tick_labels,   # date + hour for all ticks
-        tickangle=90,
-        showline=False,         # no black line
-        showspikes=True,       # no vertical blue line
-        spikecolor='rgb(128,128,128)',
-        range=[start_date, end_date],
-        side='bottom'
-    ),
-    yaxis_title="Dirección del Viento (º)",
-    showlegend=False
-)
-
-st.plotly_chart(fig, width="stretch")
-
 ## ----------------------------------------
 # Air Temperature
 ## ----------------------------------------
@@ -676,6 +590,7 @@ st.plotly_chart(fig, width="stretch")
 # -----------------------------
 st.markdown("---")
 st.caption("Powered by Streamlit • Plotly • NetCDF • Python")
+
 
 
 
