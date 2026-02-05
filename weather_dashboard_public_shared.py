6c81acf017c5249b7e8e28115627fd03035f159e
coastalpr <78867068+coastalpr@users.noreply.github.com>
2026-02-05 09:51:30 -0400

    c

--- weather_dashboard_public_shared.py
+++ weather_dashboard_public_shared.py
@@ -145,7 +145,7 @@ st.markdown(
     <style>
     /* Metric value */
     div[data-testid="stMetricValue"] {
-        font-size: 1.0rem !important;
+        font-size: 1.5rem !important;
         font-weight: bold;
     }
     /* Metric label */
