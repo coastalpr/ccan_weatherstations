81ef797cdbb6fc4aa2f787e9afb1dc7a607dd703
coastalpr <78867068+coastalpr@users.noreply.github.com>
2026-02-05 17:03:30 -0400

    C

--- weather_dashboard_public_shared.py
+++ weather_dashboard_public_shared.py
@@ -302,7 +302,7 @@ def run_animation():
     while st.session_state.play:
         current_file = radar_files[st.session_state.index]
         img = radar_to_image(current_file)
-        placeholder.image(img, use_column_width=True)
+        placeholder.image(combined, width=900)
 
         st.session_state.index += 1
         if st.session_state.index >= len(radar_files):
