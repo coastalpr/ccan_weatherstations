dcd90e12442c51587e15ff412af72c4e0415f1b8
coastalpr <78867068+coastalpr@users.noreply.github.com>
2026-02-13 20:24:11 -0400

    Revised

--- weather_dashboard_public_shared.py
+++ weather_dashboard_public_shared.py
@@ -281,7 +281,7 @@ df["wind_gust"] = pd.to_numeric(df["wind_gust"], errors="coerce")
 
 df_wind = (
     df.set_index("Hora")[["wind_avg","wind_gust","wind_direction"]]
-      .resample("2T")
+      .resample("4T")
       .mean()
       .dropna()
       .reset_index()
